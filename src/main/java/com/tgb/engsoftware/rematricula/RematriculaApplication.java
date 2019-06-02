32f80af538665cfa67279aa94663eb27191c004a
Mazui00 <rmazui00@gmail.com>
2019-06-02 03:07:11 -0300

    Estrura do projeto criada

---
+++ src/main/java/com/tgb/engsoftware/rematricula/RematriculaApplication.java
@@ -0,0 +1,13 @@
+package com.tgb.engsoftware.rematricula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RematriculaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RematriculaApplication.class, args);
+    }
+
+}
