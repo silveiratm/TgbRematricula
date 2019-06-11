d55d315233531b5690ad2f29968db49f5e7a1f8b
mazui00 <rmazui00@gmail.com>
2019-06-11 20:58:52 -0300

    Inclusão do Font Awesome.

--- src/main/java/com/tgb/engsoftware/rematricula/config/WebConfig.java
+++ src/main/java/com/tgb/engsoftware/rematricula/config/WebConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        log.info("it got in the WebConfig!!!");
-        registry.addViewController("/").setViewName("home/homePage");
+//        log.info("it got in the WebConfig!!!");
+        registry.addViewController("/").setViewName("home/index");
     }
 }
